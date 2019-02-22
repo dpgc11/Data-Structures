900b455874f54ba4c604ef73f95d938dc3099001
yogeshTelang <yogesh@lemonop.com>
2019-02-22 20:55:42 +0530

    feat: Delete by KEY in LinkedList

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -47,6 +47,70 @@ public class LinkedList {
         }
     }
 
+    /* DELETION BY KEY */
+
+    // Method to delete a node in the LinkedList by KEY
+    public static LinkedList deleteByKey(LinkedList list, int key) {
+
+        // Store head node
+        Node currNode = list.head, prev = null;
+
+        //
+        // CASE 1:
+        // If head node itself holds the key to be deleted
+
+        if (currNode != null && currNode.data == key) {
+            // Changed head
+            list.head = currNode.next;
+
+            // Display the message
+            System.out.println(key + " found and deleted");
+
+            // Return the updated list
+            return list;
+        }
+
+        //
+        // CASE 2:
+        // If the key is somewhere other than at head
+        //
+
+        // Search for the key to be deleted,
+        // keep track of the previous node
+        // as it is needed to change currNode.next
+        while (currNode != null && currNode.data != key) {
+            // If currNode does not hold key
+            // continue to next node
+            prev = currNode;
+            currNode = currNode.next;
+        }
+
+        // If the key was present, it should be at currNode
+        // Therefore the currNode shall not be null
+        if (currNode != null) {
+            // Since the key is at currNode
+            // Unlink currNode from linked list
+            prev.next = currNode.next;
+
+            // Display the message
+            System.out.println(key + " found and deleted");
+        }
+
+        //
+        // CASE 3: The key is not present
+        //
+
+        // If key was not present in linked list
+        // currNode should be null
+        if (currNode == null) {
+            // Display the message
+            System.out.println(key + " not found");
+        }
+
+        // return the list
+        return list;
+    }
+
     // Driver code
     public static void main(String[] args) {
 
@@ -66,6 +130,31 @@ public class LinkedList {
         list = insert(list, 6);
         list = insert(list, 7);
         list = insert(list, 8);
+
+        // Print the LinkedList
+        printList(list);
+
+        /*
+        DELETION BY KEY
+         */
+
+        // Delete node with value 1
+        // In this case the key is ***at head***
+        deleteByKey(list, 1);
+
+        // Print the LinkedList
+        printList(list);
+
+        // Delete node with value 1
+        // In this case the key is ***in the middle***
+        deleteByKey(list, 4);
+
+        // Print the LinkedList
+        printList(list);
+
+        // Delete node with value 1
+        // In this case the key is ***not present***
+        deleteByKey(list, 10);
 
         // Print the LinkedList
         printList(list);
